4dc7338b21d79ebf0be6deca151b22b47d714ea0
Rafael Canales Páez <rafikiscp3@gmail.com>
2026-04-22 23:36:55 -0600

    Update Func.js

--- JavaScript/Func.js
+++ JavaScript/Func.js
@@ -2,38 +2,37 @@
 const Mensaj1 = `
 <div class="menu-grid">
 <p><b>Tenemos esto en el menú:</b></p><br>
-<p><b>1.-</b> Buzón de Género</p>
-<p><b>2.-</b> Correo</p>
-<p><b>3.-</b> Calendario</p>
-<p><b>4.-</b> Vinculación</p>
-<p><b>5.-</b> Bolsa de trabajo</p>
-<p><b>6.-</b> Alumnos</p>
-<p><b>7.-</b> Docentes</p>
-<p><b>8.-</b> Instituto</p>
-<p><b>9.-</b> Departamentos</p>
-<p><b>10.-</b> Admisión</p>
-<p><b>11.-</b> Oferta Educativa</p>
-<p><b>12.-</b> PRODEP</p>
+<p><b>1.-</b> Correo</p>
+<p><b>2.-</b> Calendario</p>
+<p><b>3.-</b> Vinculación</p>
+<p><b>4.-</b> Bolsa de trabajo</p>
+<p><b>5.-</b> Alumnos</p>
+<p><b>6.-</b> Docentes</p>
+<p><b>7.-</b> Instituto</p>
+<p><b>8.-</b> Departamentos</p>
+<p><b>9.-</b> Admisión</p>
+<p><b>10.-</b> Oferta Educativa</p>
+<p><b>11.-</b> PRODEP</p>
 </div>
 `;
 
 const Mensaj2 = `
 <div class="menu-grid">
-<p><b>22.-</b> Arquitectura</p>
-<p><b>23.-</b> Ingeniería Mecatrónica</p>
-<p><b>24.-</b> Ingeniería Electrónica</p>
-<p><b>25.-</b> Ingeniería en Sistemas Automotrices</p>
-<p><b>26.-</b> Ingeniería en Sistemas Computacionales</p>
-<p><b>27.-</b> Ingeniería Ferroviaria</p>
-<p><b>28.-</b> Administración</p>
-<p><b>29.-</b> Contador Público</p>
+<p><b>21.-</b> Arquitectura</p>
+<p><b>22.-</b> Ingeniería Mecatrónica</p>
+<p><b>23.-</b> Ingeniería Electrónica</p>
+<p><b>24.-</b> Ingeniería en Sistemas Automotrices</p>
+<p><b>25.-</b> Ingeniería en Sistemas Computacionales</p>
+<p><b>26.-</b> Ingeniería Ferroviaria</p>
+<p><b>27.-</b> Administración</p>
+<p><b>28.-</b> Contador Público</p>
 </div>
 `;
 
 const Mensaj5 = `
-13.- Seguro de accidentes<br>
-14.- Becas<br>
-15.- Titulación
+12.- Seguro de accidentes<br>
+13.- Becas<br>
+14.- Titulación
 `;
 
 const Mensaj6 = `
@@ -60,34 +59,77 @@ function limpiarTexto(texto) {
     return div.innerHTML;
 }
 
+//Quita acentos
+function normalizarTexto(texto) {
+    return texto
+        .toLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "");
+}
+
+function crearCorreo(nombre, correo) {
+    return `
+    <div style="
+        background: #f5f5f5;
+        padding: 10px;
+        border-radius: 10px;
+    ">
+        <div style="font-weight: bold;">${nombre}</div>
+        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=${correo}" target="_blank">
+            ${correo}
+        </a>
+    </div>
+    `;
+}
+
 // ================= RESPUESTAS PRINCIPALES =================
 const respuestas = {
-    "1": () => "<b>TeChat:</b> Aquí está el buzón de género: <a href='https://tlahuac.tecnm.mx/' target='_blank'>Enlace</a>",
-    "2": () => "<b>TeChat:</b> Abriendo correo: <a href='https://mail.google.com/' target='_blank'>Gmail</a>",
-    "3": () => {
+"1": () => {
+    return `
+    <b>TeChat:</b><br><br>
+
+    <div style="
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        gap: 15px;
+    ">
+
+        ${crearCorreo("Dirección", "direccion@tlahuac.tecnm.mx")}
+        ${crearCorreo("Sindicato", "sindicato@tlahuac.tecnm.mx")}
+        ${crearCorreo("Subdirección Académica", "sub.academica@tlahuac.tecnm.mx")}
+        ${crearCorreo("Ciencias Básicas", "c.basicas@tlahuac.tecnm.mx")}
+        ${crearCorreo("Ciencias de la Tierra", "ciencias.tierra@tlahuac.tecnm.mx")}
+        ${crearCorreo("Eléctrica y Electrónica", "electronica@tlahuac.tecnm.mx")}
+        ${crearCorreo("Sistemas y Computación", "sis.com@tlahuac.tecnm.mx")}
+        ${crearCorreo("Económico-Administrativas", "eco.admin@tlahuac.tecnm.mx")}
+
+    </div>
+    `;
+},
+    "2": () => {
         abrir_url("PDFS/Calendario 2026.pdf");
         return "<b>TeChat:</b> Aquí tienes el calendario <a href='PDFS/Calendario 2026.pdf' target='_blank'>Calendario</a>";
     },
-    "4": () => {
+    "3": () => {
         abrir_url("./revista/index2.html");
         return "<b>TeChat:</b> Abriendo revista de vinculación <a href='./revista/index2.html' target='_blank'>Revista de vinculación</a>";
     },
-    "5": () => {
+    "4": () => {
         abrir_url("https://tlahuac.tecnm.mx/BolsaTrabajo/OCC");
         return "<b>TeChat:</b> Bolsa de trabajo: <a href='https://tlahuac.tecnm.mx/BolsaTrabajo/OCC' target='_blank'>Ver bolsa de trabajo</a>"},
-    "6": () => "<b>TeChat:</b><br>" + Mensaj5,
-    "7": () => "<b>TeChat:</b><br>" + Mensaj6,
-    "8": () => {
+    "5": () => "<b>TeChat:</b><br>" + Mensaj5,
+    "6": () => "<b>TeChat:</b><br>" + Mensaj6,
+    "7": () => {
         abrir_url("./revista/index.html");
         return "<b>TeChat:</b> Información del instituto <a href='./revista/index.html' target='_blank'>Revista de Instituto</a>";
     },
-    "9": () => "<b>TeChat:</b><br>" + Mensaj7,
-    "10": () => {
+    "8": () => "<b>TeChat:</b><br>" + Mensaj7,
+    "9": () => {
         abrir_url("https://tlahuac.tecnm.mx/Admision/Admision");
         return "<b>TeChat:</b> Información de admisión";
     },
-    "11": () => "<b>TeChat:</b><br>" + Mensaj2,
-    "12": () => {
+    "10": () => "<b>TeChat:</b><br>" + Mensaj2,
+    "11": () => {
         abrir_url("https://tlahuac.tecnm.mx/DesarrolloAcademico/Dirposgrado");
         return "<b>TeChat:</b> PRODEP";
     }
@@ -95,73 +137,73 @@ const respuestas = {
 
 // ================= SUBMENÚS =================
 const subRespuestas = {
-    "13": () => {
+    "12": () => {
         abrir_url("PDFS/SEGUROS.pdf"); 
         return "<b>TeChat:</b> Seguro de accidentes: <a href='PDFS/SEGUROS.pdf' target='_blank'>Ver Seguros</a>"; 
     },
-    "14": () => { 
+    "13": () => { 
         abrir_url("PDFS/BECAS.pdf"); 
         return "<b>TeChat:</b> Becas: <a href='PDFS/BECAS.pdf' target='_blank'>Mostrar Becas</a>"; 
     },
-    "15": () => {
+    "14": () => {
         abrir_url("https://tlahuac.tecnm.mx/Departamentos/Titulacion"); 
         return "<b>TeChat:</b> Titulación: <a href='https://tlahuac.tecnm.mx/Departamentos/Titulacion' target='_blank'>Mostrar apartado de Titulación</a>"; 
     },
 
-    "16": () => { 
+    "15": () => { 
         abrir_url("./revista/inddoc.html"); 
         return "<b>TeChat:</b> Convocatoria <a href='./revista/inddoc.html' target='_blank'>Mostrar revista de convocatorias</a>"; 
     },
-    "17": () => { 
+    "16": () => { 
         abrir_url("https://tlahuac.tecnm.mx/Docentes/Investigacion"); 
         return "Investigación <a href='https://tlahuac.tecnm.mx/Docentes/Investigacion' target='_blank'>Ver apartado investigación</a>";
     },
-    "18": () => { 
+    "17": () => { 
         abrir_url("https://tlahuac.tecnm.mx/Docentes/Tutoria"); 
         return "Tutorías <a href='https://tlahuac.tecnm.mx/Docentes/Tutoria' target='_blank'>Ver apartado de tutorias</a>";
     },
 
-    "19": () => { 
+    "18": () => { 
         abrir_url("https://tlahuac.tecnm.mx/Departamentos/Subdireccion_Academica"); 
         return "Subdirección Académica <a href='https://tlahuac.tecnm.mx/Departamentos/Subdireccion_Academica' target='_blank'>Ver apartado de subdireccion academica</a>"; 
     },
-    "20": () => { 
+    "19": () => { 
         abrir_url("https://tlahuac.tecnm.mx/Departamentos/Subdireccion_Administrativa"); 
         return "Subdirección Administrativa <a href='https://tlahuac.tecnm.mx/Departamentos/Subdireccion_Administrativa' target='_blank'>Ver apartado de subdireccion academica</a>"; 
     },
-    "21": () => { 
+    "20": () => { 
         abrir_url("https://tlahuac.tecnm.mx/Departamentos/Subdireccion_de_Planeacion_y_Vinculacion"); 
         return "Planeación <a href='https://tlahuac.tecnm.mx/Departamentos/Subdireccion_de_Planeacion_y_Vinculacion' target='_blank'>Ver apartado de planeación</a>"; 
     },
 
-    "22": () => { 
+    "21": () => { 
         abrir_url("./revista/indArq.html"); 
         return "Arquitectura"; 
     },
-    "23": () => { 
+    "22": () => { 
         abrir_url("./revista/indMec.html"); 
         return "Mecatrónica"; 
     },
-    "24": () => { 
+    "23": () => { 
         abrir_url("./revista/indiElec.html"); 
         return "Electrónica"; 
     },
-    "25": () => { 
+    "24": () => { 
         abrir_url("./revista/indiAu.html"); 
         return "Automotrices"; 
     },
-    "26": () => { 
+    "25": () => { 
         abrir_url("./revista/indSisCom.html"); 
         return "Sistemas Computacionales"; },
-    "27": () => { 
+    "26": () => { 
         abrir_url("./revista/indiFerr.html"); 
         return "Ferroviaria"; 
     },
-    "28": () => { 
+    "27": () => { 
         abrir_url("./revista/indiAdmin.html"); 
         return "Administración"; 
     },
-    "29": () => { 
+    "28": () => { 
         abrir_url("./revista/indiConta.html"); 
         return "Contador"; 
     }
@@ -172,6 +214,34 @@ const palabrasClave = {
     "hola": () => "¡Hola! 😊",
     "menu": () => Mensaj1,
     "contacto": () => "Teléfono: 7312-5614 | 5841-0560",
+    "calendario": () => respuestas["2"](),
+    "vinculacion": () => respuestas["3"](),
+    "correo de direccion": () => `
+        <b>TeChat:</b><br><br>
+        ${crearCorreo("Dirección", "direccion@tlahuac.tecnm.mx")}
+    `,
+    "correo": () => respuestas["1"](),
+    "": () => "",
+    "": () => "",
+    "": () => "",
+    "": () => "",
+    "": () => "",
+    "": () => "",
+    "": () => "",
+    "": () => "",
+    "": () => "",
+    "": () => "",
+    "": () => "",
+    "": () => "",
+    "": () => "",
+    "": () => "",
+    "": () => "",
+    "": () => "",
+    "": () => "",
+    "": () => "",
+    "": () => "",
+    "": () => "",
+    "": () => "",
     "salir": () => { abrir_url("https://tlahuac.tecnm.mx/"); return "Hasta luego"; },
     "sapei": () => { abrir_url("https://sapei.tlahuac.tecnm.mx/"); return "Abriendo SAPEI"; }
 };
@@ -192,7 +262,7 @@ function procesarEntrada() {
         return;
     }
 
-    input = limpiarTexto(input).toLowerCase();
+    input = normalizarTexto(limpiarTexto(input));
 
     let chatBox = document.getElementById("chat");
 
@@ -200,13 +270,46 @@ function procesarEntrada() {
 
     let respuesta = "";
 
-    // 🔹 PRIORIDAD: NÚMEROS
-    if (respuestas[input]) {
-        respuesta = respuestas[input]();
-    } else if (subRespuestas[input]) {
-        respuesta = subRespuestas[input]();
-    } else {
-        // 🔹 PALABRAS CLAVE
+    // ================= 🧠 DETECCIÓN INTELIGENTE DE CORREOS =================
+    const departamentos = {
+        "direccion": "direccion@tlahuac.tecnm.mx",
+        "sindicato": "sindicato@tlahuac.tecnm.mx",
+        "subdireccion academica": "sub.academica@tlahuac.tecnm.mx",
+        "ciencias basicas": "c.basicas@tlahuac.tecnm.mx",
+        "ciencias de la tierra": "ciencias.tierra@tlahuac.tecnm.mx",
+        "electronica": "electronica@tlahuac.tecnm.mx",
+        "sistemas": "sis.com@tlahuac.tecnm.mx",
+        "economico administrativas": "eco.admin@tlahuac.tecnm.mx"
+    };
+
+    // Detecta frases como:
+    // "correo direccion", "correo de direccion", "correo electronico de direccion"
+    if (input.includes("correo")) {
+        for (let depto in departamentos) {
+            if (input.includes(depto)) {
+                respuesta = `
+                <b>TeChat:</b><br><br>
+                ${crearCorreo(
+                    depto.charAt(0).toUpperCase() + depto.slice(1),
+                    departamentos[depto]
+                )}
+                `;
+                break;
+            }
+        }
+    }
+
+    // ================= 🔢 PRIORIDAD: NÚMEROS =================
+    if (respuesta === "") {
+        if (respuestas[input]) {
+            respuesta = respuestas[input]();
+        } else if (subRespuestas[input]) {
+            respuesta = subRespuestas[input]();
+        }
+    }
+
+    // ================= 🔑 PALABRAS CLAVE =================
+    if (respuesta === "") {
         for (let key in palabrasClave) {
             if (input.includes(key)) {
                 respuesta = palabrasClave[key]();
@@ -215,11 +318,12 @@ function procesarEntrada() {
         }
     }
 
+    // ================= ❌ DEFAULT =================
     if (respuesta === "") {
         respuesta = "<b>TeChat:</b> No entiendo esa opción";
     }
 
-    // Mostrar en chat
+    // ================= 💬 MOSTRAR =================
     chatBox.innerHTML += `<p><b>Tú:</b> ${input}</p>`;
     chatBox.innerHTML += `<p>${respuesta}</p>`;
     chatBox.scrollTop = chatBox.scrollHeight;
